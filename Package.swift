020bea6bfb9de22fb2b866eee68fa5e3168a8355
Lennard Kittner <lennardkittner@icloud.com>
2022-10-23 23:23:45 +0200

    syntax fix

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SystemKit",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other 
-packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SystemKit",
             targets: ["SystemKit"]),
@@ -18,16 +17,14 @@ packages.
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends 
-on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SystemKit",
             dependencies: [],
             path: "SystemKit"),
         .testTarget(
             name: "SystemKitTests",
-            dependencies: ["SystemKit"]
+            dependencies: ["SystemKit"],
             path: "SystemKitTests"),
     ]
 )
-
